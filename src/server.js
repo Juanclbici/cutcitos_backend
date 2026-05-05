c4f3228b2cd6d659454629a73de53ea57c809c02
Juanclbici <juan.lopez4572@alumnos.udg.mx>
2026-05-04 22:02:11 -0600

    Update server startup logs and dotenv configuration
    Removed database connection log and adjusted server message.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const app = require('./app');
 const db = require('./models');
-
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
+// Quitamos el .sync() para evitar el error de Duplicate Foreign Key
 app.listen(PORT, () => {
-  console.log(`Servidor en http://localhost:${PORT}`);
-  console.log('Conexión a la base de datos establecida.');
+  console.log(`Servidor corriendo`);
 });
