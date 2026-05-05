d109eb277987601400a758d5f5648da4faed1820
Juanclbici <juan.lopez4572@alumnos.udg.mx>
2026-05-04 22:08:29 -0600

    Refactor Sequelize setup and model import logic
    Refactor Sequelize initialization and model loading logic.

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const fs = require('path');
+const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
@@ -11,30 +11,24 @@ const config = require(__dirname + '/../config/database.js')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
+sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: config.dialect,
+  logging: config.logging,
+  timezone: config.timezone,
+  dialectOptions: config.dialectOptions
+});
 
 fs.readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
+  .filter(file => (file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'))
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
 Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+  if (db[modelName].associate) db[modelName].associate(db);
 });
 
 db.sequelize = sequelize;
