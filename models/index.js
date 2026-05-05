019885f27aa6fc3229fe355192948b0b6091010f
Juanclbici <juan.lopez4572@alumnos.udg.mx>
2026-05-04 22:01:16 -0600

    Refactor fs import to use path module

--- models/index.js
+++ models/index.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const fs = require('fs');
+const fs = require('path');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+
 const config = require(__dirname + '/../config/database.js')[env];
 const db = {};
 
@@ -16,8 +17,7 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
+fs.readdirSync(__dirname)
   .filter(file => {
     return (
       file.indexOf('.') !== 0 &&
